f01db1a7ef303786ea0972181569aa4fd4ee0510
Paul Charlton <paul.charlton.74@gmail.com>
2017-11-26 17:39:31 -0800

    build weekly winners system notice

--- src/main/java/com/ocsports/reports/PublishedWinnersReport.java
+++ src/main/java/com/ocsports/reports/PublishedWinnersReport.java
@@ -2,23 +2,33 @@ package com.ocsports.reports;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+
 import com.ocsports.core.PropertiesHelper;
 import com.ocsports.core.SportTypes;
+import com.ocsports.models.LeagueSeriesXrefModel;
 import com.ocsports.models.SeasonModel;
+import com.ocsports.models.SeasonSeriesModel;
 import com.ocsports.sql.PoolSQLController;
 import com.ocsports.sql.SeasonSQLController;
 
 
+/**
+ * This report class will automatically build a system notice based on
+ * the winners and high scores for each series in the active season, for
+ * the default league. This will avoid an administrator logging in to check
+ * the winners and manually update the correct system notice each week.
+ */
 public class PublishedWinnersReport extends BaseReport {
 
-    Collection seasonWinners = null;
+    private String systemNotice = "";
 
     public PublishedWinnersReport() {
         super(); // set/use report defaults
     }
 
     public String getSystemNotice() {
-        return null;
+        return this.systemNotice;
     }
 
     public boolean build() {
@@ -35,7 +45,10 @@ public class PublishedWinnersReport extends BaseReport {
             int activeSeasonId = ((SeasonModel)seasons.get(0)).getId();
             int defaultLeagueId = PropertiesHelper.getDefaultLeagueId();
 
-            seasonWinners = poolSql.getLeagueSeriesBySeason(defaultLeagueId, activeSeasonId);
+            Collection seriesModels = seasonSql.getSeriesBySeason(activeSeasonId);
+            Collection leagueSeriesModels = poolSql.getLeagueSeriesBySeason(defaultLeagueId, activeSeasonId);
+
+            buildNotice(leagueSeriesModels, seriesModels);
         }
         catch(Exception e) {
             return buildFailed();
@@ -53,9 +66,45 @@ public class PublishedWinnersReport extends BaseReport {
         return buildSuccessful();
     }
 
+    public void buildNotice(Collection leagueSeriesModels, Collection seriesModels) {
+        // reset the message before we rebuild
+        this.systemNotice = "";
+
+        if(leagueSeriesModels == null || leagueSeriesModels.size() == 0) {
+            return;
+        }
+
+        String msg = "";
+        Iterator iter = leagueSeriesModels.iterator();
+        while(iter.hasNext()) {
+            LeagueSeriesXrefModel lsx = (LeagueSeriesXrefModel)iter.next();
+            int seq = this.getSeriesSequence(seriesModels, lsx.getSeriesId());
+            String week = "Week " + String.valueOf(seq) + " Winners - " +
+                      "<strong>" + lsx.getLeagueWinners() + "</strong>" +
+                      " (" + lsx.getLeagueHighScore() + " Wins)";
+
+            // later series go on top of the list; <br> tag between
+            if(msg.length() > 0) msg = "<br>" + msg;
+            msg = week + msg;
+        }
+        // put the final touches on the winners list
+        this.systemNotice = "<span style='color:#3355FF'>" + msg + "</span>";
+    }
+
+    private int getSeriesSequence(Collection seriesModels, int seriesId) {
+        if(seriesModels == null || seriesModels.size() == 0) return 0;
+
+        Iterator iter = seriesModels.iterator();
+        while(iter.hasNext()) {
+            SeasonSeriesModel ssm = (SeasonSeriesModel)iter.next();
+            if(ssm.getId() == seriesId) return ssm.getSequence();
+        }
+        return 0;
+    }
+
     public BaseReport copy() {
         PublishedWinnersReport rpt = new PublishedWinnersReport();
-        rpt.seasonWinners = this.seasonWinners;
+        rpt.systemNotice = this.systemNotice;
         return rpt;
     }
 }
--- src/main/webapp/jsp/common/published_notices.jsp
+++ src/main/webapp/jsp/common/published_notices.jsp
@@ -1,36 +1,43 @@
 <%@page contentType="text/html" %>
-<%@page import="java.util.Collection"%>
+<%@page import="java.util.ArrayList"%>
 <%@page import="java.util.Iterator"%>
 <%@page import="com.ocsports.models.SystemNoticeModel"%>
 <%@page import="com.ocsports.reports.PublishedNoticesReport"%>
 <%@page import="com.ocsports.reports.PublishedWinnersReport"%>
 <%@page import="com.ocsports.reports.ReportHelper"%>
 <%
-    Collection notices = null;
+    ArrayList noticeList = new ArrayList();
+
+    // first, gather any published system notices and add to the list
     Object rpt = ReportHelper.getReport(ReportHelper.RPT_PUBLISHED_NOTICES);
     if(rpt != null) {
-        notices = (PublishedNoticesReport)rpt.getNotices();
+        Collection sysNotices = (PublishedNoticesReport)rpt.getNotices();
+        if(sysNotices != null && sysNotices.size() > 0) {
+            Iterator sysIter = sysNotices.iterator();
+            while(sysIter.hasNext()) {
+                SystemNoticeModel snm = (SystemNoticeModel)sysIter.next();
+                noticeList.add(ssm.getMessage());
+            }
     }
-    if(notices == null) notices = new ArrayList();
 
+    // see if there is a weekly winners notice, and add that to the list
     rpt = ReportHelper.getReport(ReportHelper.RPT_PUBLISHED_WINNERS);
     if(rpt != null) {
-        String sysNotice = (PublishedWinnersReport)rpt.getSystemNotice();
-        if(sysNotice != null && sysNotice.length() > 0) notices.add(sysNotice);
+        String notice = (PublishedWinnersReport)rpt.getSystemNotice();
+        if(notice != null && notice.length() > 0) noticeList.add(notice);
     }
 %>
 <h4>Recent News / League Information</h4>
 <div id="msgList" style="width:100%; overflow:auto">
     <ul>
-        <%if(notices.size() == 0) {%>
+        <%if(noticeList.size() == 0) {%>
             <li>No messages</li>
         <%} else { %>
             <%
-            Iterator iter = notices.iterator();
+            Iterator iter = noticeList.iterator();
             while(iter.hasNext()) {
-                SystemNoticeModel snm = (SystemNoticeModel)iter.next();
                 %>
-                <li><%=snm.getMessage()%></li>
+                <li><%=(String)iter.next()%></li>
                 <%
             }
             %>
